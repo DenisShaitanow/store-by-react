20556f52f5ba492772d34a4ea446fefde6a5f687
DenisShaitanow <shaitanow1992@yandex.ru>
2025-12-02 19:44:30 +0300

    02.12.2025 18.44

--- src/pages/formOder/FormOrder.tsx
+++ src/pages/formOder/FormOrder.tsx
@@ -1,5 +1,5 @@
  import styles from './FormOder.module.css';
-import { useEffect, useState, type ChangeEvent, type FC } from 'react';
+import { useEffect, useLayoutEffect, useState, type ChangeEvent, type FC } from 'react';
 import { InputUI } from '../../ui/input';
 import { InputDropDown } from '../../ui/inputDropDown/imputDropDownSimple';
 import { ButtonUI } from '../../ui/button';
@@ -19,10 +19,21 @@ const formatCardNumber = (inputValue: string) => {
     return formattedValue;
 };
 
+interface IFormData {
+    selectСourier: boolean;
+    adress: string;
+    adressPoint: string;
+    formPaySelf: boolean;
+    numberCard: string;
+    PersonCard: string;
+    CVV: string;
+}
+
 
 const FormOderPage: FC = () => { 
 
-    const [formData, setFormData] = useState({
+    const storedFormDataString = JSON.parse(localStorage.getItem('orderForm') ?? '');
+    const initialFormData: IFormData = storedFormDataString ? storedFormDataString : {
         selectСourier: true,
         adress: '',
         adressPoint: '',
@@ -30,65 +41,59 @@ const FormOderPage: FC = () => {
         numberCard: '',
         PersonCard: '',
         CVV: ''
-    })
+    };
 
-    const [selectСourier, setSelectCourier] = useState<boolean>(true);
-    const [adress, setAdress] = useState<string>('');
-    const [adressPoint, setAdressPoint] = useState<string>('');
-    const [formPaySelf, setFormPay] = useState<boolean>(true);
-    const [numberCard, setNumber] = useState<string>('');
-    const [PersonCard, setPerson] = useState<string>('');
-    const [CVV, setCVV] = useState<string>('');
-    const [buttonBuy, setButtonBuy] = useState<boolean>(true);
+    const [formData, setFormData] = useState<IFormData>(initialFormData);
+
+
+    const [buttonBuyDisabled, setButtonBuyDisabled] = useState<boolean>(true);
 
     const handleChangeNumberCard = (e: ChangeEvent<HTMLInputElement>) => {
         const rawValue = e.target.value;
         const cleanedAndFormattedValue = formatCardNumber(rawValue); // Форматируем значение
-        setNumber(cleanedAndFormattedValue); // Устанавливаем новое состояние
+        setFormData(prev => ({...prev, numberCard: cleanedAndFormattedValue}));
        
     }
 
     const handleChangePersonCard = (e: ChangeEvent<HTMLInputElement>) => {
-        setPerson(e.target.value);
         setFormData(prev => ({...prev, PersonCard: e.target.value as string}));
-        localStorage.setItem('orderForm', JSON.stringify(formData))
     }
 
     const handleChangeCVV = (e: ChangeEvent<HTMLInputElement>) => {
-        setCVV(e.target.value);
+        setFormData(prev => ({...prev, CVV: e.target.value}));
     }
 
     const handleChangeAdress = (e: ChangeEvent<HTMLInputElement>) => {
-        setAdress(e.target.value)
+        setFormData(prev => ({...prev, adress: e.target.value}));
     }
 
     const handleChangePoint = (e: ChangeEvent<HTMLInputElement>) => {
-        setAdressPoint(e.target.value);
+        setFormData(prev => ({...prev, adressPoint : e.target.value}));
     }
 
     useEffect(() => {
         // Проверяем общую доступность формы
         const isValidAddressSelection =
-            (selectСourier && adress.trim()) ||       
-            (!selectСourier && adressPoint.trim()); 
+            (formData.selectСourier && formData.adress.trim()) ||       
+            (!formData.selectСourier && formData.adressPoint.trim()); 
     
         const isPaymentDataComplete =
-            formPaySelf ||
-            (!formPaySelf &&
-                numberCard.trim() !== "" &&
-                PersonCard.trim() !== "" &&
-                CVV.trim() !== "");
+        formData.formPaySelf ||
+            (!formData.formPaySelf &&
+                formData.numberCard.trim() !== "" &&
+                formData.PersonCard.trim() !== "" &&
+                formData.CVV.trim() !== "");
     
         // Блокировка кнопки, если данные заполнены некорректно
-        setButtonBuy(!(isValidAddressSelection && isPaymentDataComplete));
+        setButtonBuyDisabled(!(isValidAddressSelection && isPaymentDataComplete));
     }, [
-        selectСourier,
-        adress,
-        adressPoint,
-        formPaySelf,
-        numberCard,
-        PersonCard,
-        CVV
+        formData
+    ]);
+
+    useEffect(() => {
+        localStorage.setItem('orderForm', JSON.stringify(formData));
+    }, [
+        formData
     ]);
 
     const handleBuy = () => {
@@ -99,34 +104,46 @@ const FormOderPage: FC = () => {
         <div className={styles.container}>
             <span className={styles.selectPay}>Выберете способ получения</span>
             <div className={styles.twoButtons}>
-                <div className={`${styles.selectPayButton} ${!selectСourier ? styles.select : '' }`} onClick={() => setSelectCourier(false)}>Самовывоз</div>
-                <div className={`${styles.selectPayButton} ${selectСourier ? styles.select : '' }`} onClick={() => setSelectCourier(true)}>Курьер</div>
+                <div className={`${styles.selectPayButton} ${!formData.selectСourier ? styles.select : '' }`} onClick={() => {
+                    setFormData(prev => ({...prev, selectСourier : false}));
+                }}>
+                    Самовывоз
+                </div>
+                <div className={`${styles.selectPayButton} ${formData.selectСourier ? styles.select : '' }`} onClick={() => {
+                    setFormData(prev => ({...prev, selectСourier : true}));
+                }}>
+                    Курьер
+                </div>
             </div>
-            { selectСourier && (
-                <InputUI name='adress' type='text' title='Адрес' value={adress} onChange={handleChangeAdress} placeholder='Введите адрес'/>
+            { formData.selectСourier && (
+                <InputUI name='adress' type='text' title='Адрес' value={formData.adress} onChange={handleChangeAdress} placeholder='Введите адрес'/>
             )}
-            { !selectСourier && (
-                <InputDropDown className={styles.pointTake} id='1' title='Пункт выдачи' withInput={false} value={adressPoint} onChangeOption={handleChangePoint} placeholder='Выберите удобный пункт выдачи' options={[{value: 'ул.Мичурина, д.23', label: 'ул.Мичурина, д.23'}, {value: 'пр-т Королева, д.26', label: 'пр-т Королева, д.26'}, {value: 'пл. Ленина, д.17', label: 'пл. Ленина, д.17'}, {value: 'ул. Кирова, д.17', label: 'ул. Кирова, д.17'}, {value: 'ул. Сахарова, д.1', label: 'ул. Сахарова, д.1'}]}/>
+            { !formData.selectСourier && (
+                <InputDropDown className={styles.pointTake} id='1' title='Пункт выдачи' withInput={false} value={formData.adressPoint} onChangeOption={handleChangePoint} placeholder='Выберите удобный пункт выдачи' options={[{value: 'ул.Мичурина, д.23', label: 'ул.Мичурина, д.23'}, {value: 'пр-т Королева, д.26', label: 'пр-т Королева, д.26'}, {value: 'пл. Ленина, д.17', label: 'пл. Ленина, д.17'}, {value: 'ул. Кирова, д.17', label: 'ул. Кирова, д.17'}, {value: 'ул. Сахарова, д.1', label: 'ул. Сахарова, д.1'}]}/>
             )}
 
             <span className={styles.selectPay}>Выберете способ оплаты</span>
             <div className={styles.twoButtons}>
-                <div className={`${styles.selectPayButton} ${formPaySelf ? styles.select : '' }`} onClick={() => setFormPay(true)}>Оплата при получении</div>
-                <div className={`${styles.selectPayButton} ${!formPaySelf ? styles.select : '' }`} onClick={() => setFormPay(false)}>Оплата картой онлайн</div>
+                <div className={`${styles.selectPayButton} ${formData.formPaySelf ? styles.select : '' }`} onClick={() => {
+                    setFormData(prev => ({...prev, formPaySelf : true}));
+                    }}>Оплата при получении</div>
+                <div className={`${styles.selectPayButton} ${!formData.formPaySelf ? styles.select : '' }`} onClick={() => {
+                    setFormData(prev => ({...prev, formPaySelf : false}));
+                    }}>Оплата картой онлайн</div>
             </div>
-            { !formPaySelf && (
+            { !formData.formPaySelf && (
                 <>
                 <span className={styles.infoCard}>Введите данные карты</span>
                 <div className={styles.cardInputs}>
-                    <InputUI  name='numberCard' type='text' title='Номер карты' value={numberCard} onChange={handleChangeNumberCard} placeholder='Введите номер карты'/>
-                    <InputUI halfSize name='cvvCard' type='text' title='CVV' value={CVV} onChange={handleChangeCVV} placeholder='Введите код с обратной стороны'/>
-                    <InputUI name='personCard' type='text' title='Владелец карты' value={PersonCard} onChange={handleChangePersonCard} placeholder='Введите имя владельца'/>
+                    <InputUI  name='numberCard' type='text' title='Номер карты' value={formData.numberCard} onChange={handleChangeNumberCard} placeholder='Введите номер карты'/>
+                    <InputUI halfSize name='cvvCard' type='text' title='CVV' value={formData.CVV} onChange={handleChangeCVV} placeholder='Введите код с обратной стороны'/>
+                    <InputUI name='personCard' type='text' title='Владелец карты' value={formData.PersonCard} onChange={handleChangePersonCard} placeholder='Введите имя владельца'/>
                     
                 </div>
                 </>
             )}
              
-             <ButtonUI className={styles.buttonBuy} label='Совершить покупку' disabled={buttonBuy} onClick={handleBuy}/>
+             <ButtonUI className={styles.buttonBuy} label='Совершить покупку' disabled={buttonBuyDisabled} onClick={handleBuy}/>
             
         
         </div>
